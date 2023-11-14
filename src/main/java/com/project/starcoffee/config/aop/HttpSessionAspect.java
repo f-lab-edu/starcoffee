460df2d3f4387761c2c3074100b4f3d9163a8bd4
truelightwater <truelightwater@gmail.com>
2023-11-14 16:32:48 +0900

    멘토님 코멘트 정리

--- src/main/java/com/project/starcoffee/config/aop/HttpSessionAspect.java
+++ src/main/java/com/project/starcoffee/config/aop/HttpSessionAspect.java
@@ -12,6 +12,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpSession;
+import java.util.Arrays;
 
 @Slf4j
 @Aspect
@@ -24,19 +25,9 @@ public class HttpSessionAspect {
                 .getRequest().getSession();
         String memberId = SessionUtil.getMemberId(session);
 
-        // SessionUtil 로직 안으로 넣기 ! (null 체크)
-        if (memberId.isEmpty()) {
-            throw new HttpStatusCodeException(HttpStatus.UNAUTHORIZED, "NO_LOGIN") { };
-        }
-
         Object[] args = joinPoint.getArgs();
 
-        // 시간복잡도, args.length 를 가지고 args 를 가지고 오기
-        for (int i = 0; i < args.length; i++) {
-            if (args[i] == null) {
-                args[i] = memberId;
-            }
-        }
+        Arrays.setAll(args, i -> args[i] != null ? args[i] : memberId);
 
         Object result = joinPoint.proceed(args);
 
